dbf39255b3c5e6d6d208fc0abf3f7b429396d088
Vitoria Alvim <vitoria.nardotto@gmail.com>
2024-04-29 11:32:41 -0300

    retornando dto para user resource

--- src/main/java/br/com/dicasdeumdev/api/domain/dto/UserDTO.java
+++ src/main/java/br/com/dicasdeumdev/api/domain/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package br.com.dicasdeumdev.api.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,10 @@ public class UserDTO {
     private Integer id;
     private String name;
     private String email;
+    @JsonIgnore
+    /*
+    usando o DTO é possivel omitir informações, com a anotação @JsonIgnore
+    foi possivel omitir/ignorar a senha e não mostrar no retorno
+     */
     private String password;
 }
