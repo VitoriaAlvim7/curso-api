c55415c9f465b4e9065a365dafcb876e69548e3a
Vitoria Alvim <vitoria.nardotto@gmail.com>
2024-05-02 13:43:49 -0300

    atualização de dados

--- src/main/java/br/com/dicasdeumdev/api/services/exceptions/ObjectNotFoundWxception.java
+++
@@ -1,8 +0,0 @@
-package br.com.dicasdeumdev.api.services.exceptions;
-
-public class ObjectNotFoundWxception extends RuntimeException{
-
-    public ObjectNotFoundWxception(String message) {
-        super(message);
-    }
-}
