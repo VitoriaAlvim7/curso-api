55427f0e9f5c08ab51bc9d0406d081a9dd7cca72
Vitoria Alvim <vitoria.nardotto@gmail.com>
2024-05-02 13:44:27 -0300

    atualização de dados de Resourse Exception

--- src/main/java/br/com/dicasdeumdev/api/resources/ResourceExceptionHandler.java
+++ src/main/java/br/com/dicasdeumdev/api/resources/ResourceExceptionHandler.java
@@ -2,7 +2,7 @@ package br.com.dicasdeumdev.api.resources;
 
 import br.com.dicasdeumdev.api.resources.exceptions.StandardError;
 import br.com.dicasdeumdev.api.services.exceptions.DataIntegratyViolationException;
-import br.com.dicasdeumdev.api.services.exceptions.ObjectNotFoundWxception;
+import br.com.dicasdeumdev.api.services.exceptions.ObjectNotFoundException;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
@@ -14,8 +14,8 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-    @ExceptionHandler(ObjectNotFoundWxception.class)
-    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundWxception ex, HttpServletRequest request) {
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException ex, HttpServletRequest request) {
         StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 
