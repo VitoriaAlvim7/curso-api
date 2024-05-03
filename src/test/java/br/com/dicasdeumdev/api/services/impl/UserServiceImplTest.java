981878acc07a3b380dc1afcf6ae802c984e7fbe2
Vitoria Alvim <vitoria.nardotto@gmail.com>
2024-05-03 09:50:55 -0300

    teste para classe user service metodo update com sucesso

--- src/test/java/br/com/dicasdeumdev/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/dicasdeumdev/api/services/impl/UserServiceImplTest.java
@@ -121,7 +121,17 @@ class UserServiceImplTest {
     }
 
     @Test
-    void update() {
+    void WhenUpdateThenReturnSuccess() {
+        when(repository.save(any())).thenReturn(user);
+
+        User response = service.update(userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
     }
 
     @Test
